2deae515bf20a60dbcdd4e828a8dbfafad4394b2
Ewelina Bierć <erbierc@gmail.com>
2025-10-04 12:01:33 +0200

    Refactor poem processing to use multiprocessing and improve code structure

--- parrot_brain.py
+++ parrot_brain.py
@@ -2,47 +2,58 @@ import string
 import kagglehub
 import pandas as pd
 import numpy as np
+from multiprocessing import Pool
+import os
 
-path = kagglehub.dataset_download("tgdivy/poetry-foundation-poems")
-
-df = pd.read_csv(path + "/PoetryFoundationData.csv", encoding='utf8')
-poems_col = df.Poem
-words = []
-
-for poem in poems_col:
+def cleanUp(poem):
     poem = poem.lower()
     poem = poem.strip()
     poem = poem.replace("\r\n", "")
     poem = poem.translate(str.maketrans('', '', string.punctuation + "“”‘’—–"))
     poem = poem.split()
-    words.extend(poem)
+    return poem
 
-d = {}
+if __name__ == "__main__":
+    path = kagglehub.dataset_download("tgdivy/poetry-foundation-poems")
 
-for word in words:
-    if word in d:
-        d[word] += 1
-    else:
-        d[word] = 1
+    df = pd.read_csv(path + "/PoetryFoundationData.csv", encoding='utf8')
+    poems_col = df.Poem
+    words = []
 
-l = len(words)
+    pool = Pool(os.cpu_count())
+    results = pool.map(cleanUp, poems_col)
+    pool.close()
+    pool.join()
 
-for word in d:
-    d[word] = d[word]/l
-    
-words = list(d.keys())
-probabilities = list(d.values())
+    for arr in results:
+        words.extend(arr)
 
-rng = np.random.default_rng()
-words = rng.choice(words, size=100, p=probabilities)
+    d = {}
 
-with open("poem.txt", "w", encoding="utf8") as text_file:
-    line_break = np.random.randint(8, 11)
-    counter = 1
     for word in words:
-        text_file.write(word + " ")
-        if counter == line_break:
-            text_file.write("\n")
-            counter = 1
-            line_break = np.random.randint(8, 11)
-        counter+=1
+        if word in d:
+            d[word] += 1
+        else:
+            d[word] = 1
+
+    l = len(words)
+
+    for word in d:
+        d[word] = d[word]/l
+        
+    words = list(d.keys())
+    probabilities = list(d.values())
+
+    rng = np.random.default_rng()
+    words = rng.choice(words, size=100, p=probabilities)
+
+    with open("poem.txt", "w", encoding="utf8") as text_file:
+        line_break = np.random.randint(8, 11)
+        counter = 1
+        for word in words:
+            text_file.write(word + " ")
+            if counter == line_break:
+                text_file.write("\n")
+                counter = 1
+                line_break = np.random.randint(8, 11)
+            counter+=1
